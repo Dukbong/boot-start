ed1bb9236b8ba5a7f1baaf7182d8fcf8b5bd1961
jang <gorae930301@gmail.com>
2023-08-29 00:36:18 +0900

    Servlet Session 사용

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -3,6 +3,7 @@ package hello.login.web.login;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -51,7 +53,7 @@ public class LoginController {
 		
 		return "redirect:/";
 	}
-	@PostMapping("/login")
+//	@PostMapping("/login")
 	public String loginV2(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 		if(bindingResult.hasErrors()) {
 			log.info("errros = {}", bindingResult);
@@ -71,14 +73,48 @@ public class LoginController {
 		
 		return "redirect:/";
 	}
+	@PostMapping("/login")
+	public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+		if(bindingResult.hasErrors()) {
+			log.info("errros = {}", bindingResult);
+			return "/login/loginForm";
+		}
+		
+		Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+		if(loginMember == null) {
+			bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+			return "/login/loginForm";
+		}
+		
+		// 로그인 성공 처리
+		
+		// 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+		HttpSession session = request.getSession();
+		// getSession(true) : 세션이 있으면 기존 세션 반환 / 없으면 새로운 세션 생성후 반환
+		// >> true가 기본값이다 / 생략 가능
+		// getSession(false) : 세션이 있으면 기존 세션 반환 / 없으면 null 반환
+		
+		// 세션에 로그인 회원 정보 보관
+		session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+		return "redirect:/";
+	}
 	
 //	@PostMapping("/logout")
 	public String logoutV1(HttpServletResponse response) {
 		expireCookie(response, "memberId");
 		return "redirect:/";
 	}
-	@PostMapping("/logout")
+//	@PostMapping("/logout")
 	public String logoutV2(HttpServletRequest request) {
+		sessionManager.expire(request);
+		return "redirect:/";
+	}
+	@PostMapping("/logout")
+	public String logoutV3(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			session.invalidate(); // 세션 삭제 하는 메소드
+		}
 		sessionManager.expire(request);
 		return "redirect:/";
 	}
