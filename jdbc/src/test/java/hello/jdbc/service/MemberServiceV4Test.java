004a4d87e228c9b2aa5d83b17cbe32422e1ecf05
jang <gorae930301@gmail.com>
2023-09-17 15:07:43 +0900

    jdbc 반복문제 해결 - jdbcTemplate

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -44,7 +44,8 @@ public class MemberServiceV4Test {
 		@Bean
 		MemberRepository memberRepository() {
 //			return new MemberRepositoryV4_1(dataSource);
-			return new MemberRepositoryV4_2(dataSource);
+//			return new MemberRepositoryV4_2(dataSource);
+			return new MemberRepositoryV5(dataSource);
 		}
 		
 		@Bean
